ffbdf4cce7aa96f772aabfec45cd90386c350237
songzuozhen_glb <songzuozhen_glb@kgc.com>
2020-12-17 15:29:18 +0800

    修改平台属性

---
+++ kmall-manager-service/src/main/java/com/kgc/kmall/manage/service/AttrServiceImpl.java
@@ -0,0 +1,64 @@
+package com.kgc.kmall.manage.service;
+
+import com.kgc.kmall.bean.PmsBaseAttrInfo;
+import com.kgc.kmall.bean.PmsBaseAttrInfoExample;
+import com.kgc.kmall.bean.PmsBaseAttrValue;
+import com.kgc.kmall.bean.PmsBaseAttrValueExample;
+import com.kgc.kmall.manage.mapper.PmsBaseAttrInfoMapper;
+import com.kgc.kmall.manage.mapper.PmsBaseAttrValueMapper;
+import com.kgc.kmall.service.AttrService;
+import net.bytebuddy.asm.Advice;
+import org.apache.dubbo.config.annotation.Service;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Component
+@Service
+public class AttrServiceImpl implements AttrService {
+    @Resource
+    PmsBaseAttrInfoMapper pmsBaseAttrInfoMapper;
+    @Resource
+    PmsBaseAttrValueMapper pmsBaseAttrValueMapper;
+
+    @Override
+    public List<PmsBaseAttrInfo> select(Long catalog3Id) {
+        PmsBaseAttrInfoExample example = new PmsBaseAttrInfoExample();
+        if (catalog3Id != 0 && catalog3Id != null) {
+            example.createCriteria().andCatalog3IdEqualTo(catalog3Id);
+        }
+        return pmsBaseAttrInfoMapper.selectByExample(example);
+    }
+
+    @Override
+    public Integer add(PmsBaseAttrInfo attrInfo) {
+        try {
+            if (attrInfo.getId() == null) {
+                //添加
+                pmsBaseAttrInfoMapper.insert(attrInfo);
+            } else {
+                //修改  并删除
+                pmsBaseAttrInfoMapper.updateByPrimaryKeySelective(attrInfo);
+                PmsBaseAttrValueExample example = new PmsBaseAttrValueExample();
+                example.createCriteria().andAttrIdEqualTo(attrInfo.getId());
+                pmsBaseAttrValueMapper.deleteByExample(example);
+            }
+            
+            pmsBaseAttrValueMapper.insertBatch(attrInfo.getId(), attrInfo.getAttrValueList());
+            return 1;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return -1;
+        }
+
+    }
+
+    @Override
+    public List<PmsBaseAttrValue> getAttrValueList(Long attrId) {
+        PmsBaseAttrValueExample example = new PmsBaseAttrValueExample();
+        example.createCriteria().andAttrIdEqualTo(attrId);
+        return pmsBaseAttrValueMapper.selectByExample(example);
+    }
+
+}
---
+++ kmall-manager-web/src/main/java/com/kgc/kmall/manager/controller/AttrController.java
@@ -0,0 +1,38 @@
+package com.kgc.kmall.manager.controller;
+
+import com.kgc.kmall.bean.PmsBaseAttrInfo;
+import com.kgc.kmall.bean.PmsBaseAttrValue;
+import com.kgc.kmall.service.AttrService;
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+public class AttrController {
+    @Reference
+    AttrService attrService;
+
+    @RequestMapping("/attrInfoList")
+    public List<PmsBaseAttrInfo> attrInfoList(Long catalog3Id) {
+        List<PmsBaseAttrInfo> select = attrService.select(catalog3Id);
+        return select;
+    }
+
+    @RequestMapping("/saveAttrInfo")
+    public Integer add(@RequestBody PmsBaseAttrInfo attrInfo) {
+        Integer add = attrService.add(attrInfo);
+        return add;
+    }
+
+    @RequestMapping("/getAttrValueList")
+    public List<PmsBaseAttrValue> getAttrValueList(Long attrId) {
+        List<PmsBaseAttrValue> attrValueList = attrService.getAttrValueList(attrId);
+        return attrValueList;
+    }
+
+}
